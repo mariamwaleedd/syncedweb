dfa831af1fc9366eeb257cc07986320d1dcf48d7
mariamwaleed05 <mariam.waleed2005@gmail.com>
2026-03-24 10:22:39 +0200

    feat: added routing

--- src/Routing.jsx
+++ src/Routing.jsx
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 const Routing = () => {
     return ( 
-        <>
+    <Router>
         
-        
-        
-        </>
+4<Route>
+
+</Route>
+
+    </Router>
      );
 }
  
