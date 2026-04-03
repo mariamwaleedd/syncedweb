f41c8fb4d611db60a45ffd543bfc4dc9bc828691
mariamwaleed05 <mariam.waleed2005@gmail.com>
2026-04-03 20:03:22 +0200

    feat: added lazy load to routing andimported in index.html head tags , to optimatze and fasten loading

--- src/Routing.jsx
+++ src/Routing.jsx
@@ -1,23 +1,23 @@
-
-import React, { useEffect } from 'react';
+import React, { useEffect, Suspense, lazy } from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Error from './pages/Error';
-import ContactUs from './pages/ContactUs';
-import FAQPage from './pages/FAQPage';
-import PlansPage from './pages/PlansPage';
-import Features from './pages/Features';
 import NavBar from './common/NavBar';
 import Footer from './common/Footer';
-import AboutUs from './pages/AboutUs';
-import PrivacyPolicy from './pages/PrivacyPolicy';
 import Preloader from './common/Preloader';
-import RepeatedPage from './pages/RepeatedPage';
 import HealthCursor from './common/HealthCursor';
-import WhySynced from './pages/WhySynced';
-import Blogs from './pages/Blogs';
 import BackToTop from './common/BackToTop';
 import ChatBot from './common/ChatBot';
+
+const Home = lazy(() => import('./pages/Home'));
+const Error = lazy(() => import('./pages/Error'));
+const ContactUs = lazy(() => import('./pages/ContactUs'));
+const FAQPage = lazy(() => import('./pages/FAQPage'));
+const PlansPage = lazy(() => import('./pages/PlansPage'));
+const Features = lazy(() => import('./pages/Features'));
+const AboutUs = lazy(() => import('./pages/AboutUs'));
+const PrivacyPolicy = lazy(() => import('./pages/PrivacyPolicy'));
+const RepeatedPage = lazy(() => import('./pages/RepeatedPage'));
+const WhySynced = lazy(() => import('./pages/WhySynced'));
+const Blogs = lazy(() => import('./pages/Blogs'));
 
 const Routing = () => {
   useEffect(() => {
@@ -80,19 +80,21 @@ const Routing = () => {
       <BackToTop/>
       <ChatBot/>
       <HealthCursor/>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/ContactUs" element={<ContactUs />} />
-        <Route path="/features" element={<Features />} />
-        <Route path="/AboutUs" element={<AboutUs />} />
-        <Route path="/FAQPage" element={<FAQPage />} />
-        <Route path="/plans" element={<PlansPage />} />
-        <Route path="/privacypolicy" element={<PrivacyPolicy />} />
-        <Route path="/whysynced" element={<WhySynced />} />
-        <Route path="/blog" element={<Blogs />} />
-        <Route path="/features/:slug" element={<RepeatedPage />} />
-        <Route path="*" element={<Error />} />
-      </Routes>
+      <Suspense fallback={<div className="loading-fallback"></div>}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/ContactUs" element={<ContactUs />} />
+          <Route path="/features" element={<Features />} />
+          <Route path="/AboutUs" element={<AboutUs />} />
+          <Route path="/FAQPage" element={<FAQPage />} />
+          <Route path="/plans" element={<PlansPage />} />
+          <Route path="/privacypolicy" element={<PrivacyPolicy />} />
+          <Route path="/whysynced" element={<WhySynced />} />
+          <Route path="/blog" element={<Blogs />} />
+          <Route path="/features/:slug" element={<RepeatedPage />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </Suspense>
       <Footer />
     </>
   );
